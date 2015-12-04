65d941255ab689f5fe07be851f8411e1839fef1d
steven396 <xinwang396@outlook.com>
2015-12-04 01:34:17 +0000

    add files
    add BasedOnBerrySkyModelNarrowAndLong
    add BasedOnRayleighSkyModelNarrowAndLong

---
+++ BasedOnBerrySkyModelWideAndShort/generateTestDataForRayleigh.m
@@ -0,0 +1,30 @@
+phi_suns=[30,35.5,60,90.5,180];
+theta_suns=[30,35.5,60,69.5];
+Ts=[3.5,4,6.5];
+headDirection=0;
+test_n=0;
+test=[];
+for test_i=1:3
+    for test_j=1:4
+        for test_k=1:5
+            test_n=test_n+1;
+            phi_sun=phi_suns(test_k);
+            theta_sun=theta_suns(test_j);
+            T=Ts(test_i);
+            [response,p1_sum,p2_sum]=calculateResponseOfEveryPhotoreceptorsForRayleigh(phi_sun,theta_sun,headDirection,36,T);
+            test(test_n,:)=response;
+        end
+    end
+end
+test=test';
+testResults=net(test);
+testResults=testResults';
+references=[30,35.5,60,90.5,180];
+wholeReferences=[];
+for test_q=1:12
+wholeReferences=[wholeReferences,references];
+end
+wholeReferences=wholeReferences';
+dif=abs(testResults-wholeReferences);
+finalResults=[testResults,wholeReferences,dif];
+            
